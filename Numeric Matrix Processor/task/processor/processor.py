dacc32e5243e20072868bf15333b3ded7d076bed
alcherniaev <playtowin19@gmail.com>
2020-12-09 22:44:07 +0300

    add mapping to input and reading matrix

--- Numeric Matrix Processor/task/processor/processor.py
+++ Numeric Matrix Processor/task/processor/processor.py
@@ -1,3 +1,10 @@
-n, m = input().split()
+n, m = list(map(int, input().split()))
+matrix_1 = []
+for i in range(n):
+    matrix_1.append(input())
 
+print(matrix_1)
+
+'''n_1, m_1 = input().split()
 print(n)
+'''
