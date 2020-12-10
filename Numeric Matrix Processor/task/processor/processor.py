637e65fcf0b0f755ce133b74083024843e96fc87
alcherniaev <playtowin19@gmail.com>
2020-12-10 22:34:47 +0300

    matrix-lists sum

--- Numeric Matrix Processor/task/processor/processor.py
+++ Numeric Matrix Processor/task/processor/processor.py
@@ -1,10 +1,25 @@
 n, m = list(map(int, input().split()))
 matrix_1 = []
 for i in range(n):
-    matrix_1.append(input())
+    matrix_1.append(list(map(int, input().split())))
 
-print(matrix_1)
+n_1, m_1 = list(map(int, input().split()))
+matrix_2 = []
+for i in range(n_1):
+    matrix_2.append(list(map(int, input().split())))
+#matrix_sum = [x + y for x, y in zip(matrix_1, matrix_2)]
+#matrix_sum = [[0 for i in range(n)] for j in range(m)]
 
-'''n_1, m_1 = input().split()
-print(n)
-'''
+matrix = [input().split() for i in range(row)]
+matrix_sum_0 = []
+matrix_sum_1 = []
+matrix_sum = matrix_sum_0 + matrix_sum_1
+for x, y in zip(matrix_1[:n/2], matrix_2[:n/2]):
+    for x_, y_ in zip(x, y):
+        matrix_sum_0.append(x_+y_)
+
+for x, y in zip(matrix_1[n/2:], matrix_2[n/2:]):
+    for x_, y_ in zip(x, y):
+        matrix_sum_1.append(x_+y_)
+
+print(matrix_sum)
