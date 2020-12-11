bcaa03aad879126a69694c167c41d827b9d0ca6e
alcherniaev <playtowin19@gmail.com>
2020-12-11 13:50:47 +0300

    add matrix sum processor

--- Numeric Matrix Processor/task/processor/processor.py
+++ Numeric Matrix Processor/task/processor/processor.py
@@ -5,13 +5,16 @@ for i in range(n):
 
 n_1, m_1 = list(map(int, input().split()))
 matrix_2 = []
-for i in range(n_1):
-    matrix_2.append(list(map(int, input().split())))
-#matrix_sum = [x + y for x, y in zip(matrix_1, matrix_2)]
 
-matrix_sum = [[0 for i in range(m)] for j in range(n)]
-for i in range(len(matrix_1)):
-    for j in range(len(matrix_1[0])):
-        matrix_sum[i][j] = matrix_1[i][j] + matrix_2[i][j]
+if n == n_1 and m == m_1:
+    for i in range(n_1):
+        matrix_2.append(list(map(int, input().split())))
 
-print(matrix_sum)
+    matrix_sum = [[0 for i in range(m)] for j in range(n)]
+    for i in range(len(matrix_1)):
+        for j in range(len(matrix_1[0])):
+            matrix_sum[i][j] = matrix_1[i][j] + matrix_2[i][j]
+    for i in matrix_sum:
+            print(*i)
+else:
+    print('ERROR')
