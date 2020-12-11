b3727358adc5bf0551e9f1c41a1d718d3a95e20d
alcherniaev <playtowin19@gmail.com>
2020-12-11 11:57:00 +0300

    sum works

--- Numeric Matrix Processor/task/processor/processor.py
+++ Numeric Matrix Processor/task/processor/processor.py
@@ -8,18 +8,10 @@ matrix_2 = []
 for i in range(n_1):
     matrix_2.append(list(map(int, input().split())))
 #matrix_sum = [x + y for x, y in zip(matrix_1, matrix_2)]
-#matrix_sum = [[0 for i in range(n)] for j in range(m)]
 
-matrix = [input().split() for i in range(row)]
-matrix_sum_0 = []
-matrix_sum_1 = []
-matrix_sum = matrix_sum_0 + matrix_sum_1
-for x, y in zip(matrix_1[:n/2], matrix_2[:n/2]):
-    for x_, y_ in zip(x, y):
-        matrix_sum_0.append(x_+y_)
-
-for x, y in zip(matrix_1[n/2:], matrix_2[n/2:]):
-    for x_, y_ in zip(x, y):
-        matrix_sum_1.append(x_+y_)
+matrix_sum = [[0 for i in range(m)] for j in range(n)]
+for i in range(len(matrix_1)):
+    for j in range(len(matrix_1[0])):
+        matrix_sum[i][j] = matrix_1[i][j] + matrix_2[i][j]
 
 print(matrix_sum)
