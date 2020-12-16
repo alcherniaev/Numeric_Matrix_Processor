a8e1f3d80f3112d38b3cc9293167cf4499558b91
alcherniaev <playtowin19@gmail.com>
2020-12-16 14:56:33 +0300

    lists extend

---
+++ Problems/Equation/main.py
@@ -0,0 +1,15 @@
+'''a, b, c = input().split()
+def equation_writing(a, b, c):
+    print('{}x + {} = {}'.format(a, b, c))
+
+
+equation_writing(a, b, c)'''
+
+
+
+
+
+values = ['delete me', 'do not', 'delete me']
+print(values)
+values.del[0]
+print(values)
