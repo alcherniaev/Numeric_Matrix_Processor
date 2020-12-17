542653d1d15d18ff14da870e91d251c8dcc40f33
alcherniaev <playtowin19@gmail.com>
2020-12-17 16:07:57 +0300

    lists id a bit differ than other objects

--- Problems/Black box/main.py
+++ Problems/Black box/main.py
@@ -1,1 +1,9 @@
 # use the function blackbox(lst) that is already defined
+my_own_list = list('dfdjsad')
+
+smt_ = blackbox(my_own_list)
+
+if id(my_own_list) == id(smt_):
+    print('modifies')
+else:
+    print('new')
